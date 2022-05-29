b24b062ab5d1bd4dc134b896001f41db19ea966a
acevv <acev_aleksandar@hotmail.com>
2022-05-29 12:12:21 +0200

    useRef hook added

--- login-demo/01-starting-project/src/components/Login/Login.js
+++ login-demo/01-starting-project/src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -45,6 +51,9 @@ const Login = (props) => {
 
   const ctx = useContext(AuthContext);
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
 
@@ -89,9 +98,9 @@ const Login = (props) => {
     if (formIsValid) {
       ctx.onLogin(emailState.value, passwordState.value);
     } else if (!emailIsValid) {
-
+      emailInputRef.current.focus();
     } else {
-
+      passwordInputRef.current.focus();
     }
   };
 
@@ -99,6 +108,7 @@ const Login = (props) => {
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           id="email"
           label="E-MAIL"
           type="email"
@@ -108,6 +118,7 @@ const Login = (props) => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           id="password"
           label="Password"
           type="password"
